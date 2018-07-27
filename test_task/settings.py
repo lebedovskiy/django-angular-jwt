6c8ef166cff388a14d057caefbbfd37399aeb4a4
Egor Lebedev <lebedovskiy@mail.ru>
2018-07-27 11:14:35 +0500

    init commit

--- test_task/settings.py
+++ test_task/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '=6gi554(6(^%jt9u3%bam2-#1j%g&*v8%b(q1dqi(_ew%r9tfx'
 
-# ToDo: SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
